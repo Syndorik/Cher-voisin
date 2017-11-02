5eb17eab5fca3cea8334ef2c74550e8f6e627397
Matthieu Alingrin <matthieu.alingrin@imt-atlantique.net>
2017-11-02 11:17:31 +0100

    Database validated

--- creationDB.sql
+++ creationDB.sql
@@ -3,6 +3,7 @@ drop table if exists Friends;
 drop table if exists Loans;
 drop table if exists Objects;
 drop table if exists Reviews;
+drop table if exists Godfathers;
 
 
 create table Users(
@@ -12,24 +13,37 @@ create table Users(
     first_name VARCHAR(50) not null,
     email VARCHAR(50) not null unique,
     password VARCHAR(30) not null,
-    address VARCHAR(30) not null,
+    address VARCHAR(50) null,
     tokens int not null,
     mark int not null check (mark <= 5),
-    state boolean,
-    level int,
     primary key (user_ID)
 );
 
 create table Friends(
-    user_ID1 VARCHAR(50) not null,
-    user_ID2 VARCHAR(50) not null,
+    user_ID1 int not null,
+    user_ID2 int not null,
     primary key (user_ID1, user_ID2)
 );
 
+create table Godfathers{
+    godfather_ID int not null,
+    godchild_ID int not null,
+    primary key (godfather_ID, godchild_ID)
+    }
+
+create table Titles{
+    user_ID int not null,
+    borrower_title VARCHAR(50),
+    lender_title VARCHAR(50),
+    godfather_title VARCHAR(50),
+    primary key (user_ID)
+    }
+
 create table Reviews(
     review_ID serial,
-    user_receiving VARCHAR(50) not null,
-    user_giving VARCHAR (50) not null,
+    loan_ID int not null,
+    user_receiving int not null,
+    user_giving int not null,
     mark int not null check (mark < 5),
     comment VARCHAR(200) not null,
     primary key (review_ID)
