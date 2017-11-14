1128ae8dfe97c215939774c03353400974f8a5ff
Matthieu Alingrin <matthieu.alingrin@imt-atlantique.net>
2017-11-14 23:05:52 +0100

    update trying with the logo (but expecting rounded version)

--- header.php
+++ header.php
@@ -19,6 +19,7 @@
               <span class="icon-bar"></span>
               <span class="icon-bar"></span>
           </button>
+          <a class="navbar-brand" id="logo"><img src="images/logo.png" width="20%" ></a>
       </div>
       <!-- Collect the nav links, forms, and other content for toggling -->
       <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
